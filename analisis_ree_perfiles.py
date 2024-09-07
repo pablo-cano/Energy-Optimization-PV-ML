911014d14afd0e7ddc03111985ddd3d5624ca130
Pablo Cano <pab.can.gal@gmail.com>
2024-09-07 18:02:44 +0200

    Create analisis_ree_perfiles.py

---
+++ analisis_ree_perfiles.py
@@ -0,0 +1,43 @@
+"""
+Universitat Carlemany - Bachelor en Data Science
+
+Proyecto Final de Bachelor: 
+"Optimización Económica del Almacenamiento de Energía en Sistemas Fotovoltaicos Mediante Algoritmos de Machine Learning"
+
+Autor: Pablo Felipe Cano Galán
+Descripción: Este código forma parte del trabajo de fin de grado (TFB), centrado en el desarrollo de modelos de machine learning para la predicción de momentos óptimos de carga y descarga de baterías en sistemas fotovoltaicos, con el fin de maximizar la eficiencia energética y la rentabilidad económica de dichos sistemas. El proyecto incluye la implementación de herramientas de simulación y una aplicación web para facilitar la gestión de sistemas fotovoltaicos.
+
+Fecha: 07/09/2024
+"""
+
+import pandas as pd
+from datetime import datetime
+from statsmodels.tsa.stattools import adfuller
+import plotly.express as px
+
+def Prueba_Dickey_Fuller(series , column_name):
+    print (f'Resultados de la prueba de Dickey-Fuller para columna: {column_name}')
+    dftest = adfuller(series, autolag='AIC')
+    dfoutput = pd.Series(dftest[0:4], index=['Test Statistic','p-value','No Lags Used','Número de observaciones utilizadas'])
+    for key,value in dftest[4].items():
+       dfoutput['Critical Value (%s)'%key] = value
+    print (dfoutput)
+    if dftest[1] <= 0.05:
+        print("Conclusion:====>")
+        print("Rechazar la hipótesis nula")
+        print("Los datos son estacionarios")
+    else:
+        print("Conclusion:====>")
+        print("No se puede rechazar la hipótesis nula")
+        print("Los datos no son estacionarios")
+
+df = pd.read_csv("typical_year_profile.csv")
+df.info()
+
+df['datetime'] = pd.to_datetime('2023-01-01') + pd.to_timedelta(df['day'] - 1, unit='d') + pd.to_timedelta(df['hour'], unit='h')
+
+fig = px.line(df, x=df["datetime"], y="COEF. PERFIL A",
+              title="Perfil de Consumo")
+fig.show()
+
+Prueba_Dickey_Fuller(df["COEF. PERFIL A"],"COEF. PERFIL A")
